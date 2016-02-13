899dd1e1e294adffd70f5ce4b823f8a68233e557
camilo montoya <camilomontoya2@gmail.com>
2016-02-13 12:52:00 -0500

    initial configuration

--- server/app.js
+++ server/app.js
@@ -1,10 +1,26 @@
-var express = require('express');
+// Dependencies
+var express = require('express'),
+	mongoose = require('mongoose'),
+	bodyParser = require('body-parser');
+
+// MongoDB
+mongoose.connect('mongodb://localhost/user_service_dev');
+
+
+
+// Express
 var app = express();
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
+
+//First Route
 app.get('/', function(req, res){
 	res.send("it's working!");
 });
 
+
+// Start server
 app.listen(40000);
 console.log("go to http://localhost:40000");
